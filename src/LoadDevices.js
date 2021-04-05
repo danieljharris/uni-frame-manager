e542e7d9a423c504693c7874a778f059ab498bd9
danieljharris <danieljharris26@yahoo.co.uk>
2021-04-05 17:50:44 +0100

    Changed IP to work on cloud

--- src/LoadDevices.js
+++ src/LoadDevices.js
@@ -26,7 +26,8 @@ class LoadDevices extends React.Component {
   }
 
   componentDidMount() {
-    fetch('http://UniFrame.local:235/devices')
+    // fetch('http://UniFrame.local:235/devices')
+    fetch('http://79.67.44.40:235/devices')
     .then(res => res.json())
     .then((online) => {
       this.setState({ devices: online.devices });
@@ -47,7 +48,8 @@ class LoadDevices extends React.Component {
   }
 
   handleClickRefresh() {
-    fetch('http://UniFrame.local:235/devices')
+    // fetch('http://UniFrame.local:235/devices')
+    fetch('http://79.67.44.40:235/devices')
     .then(res => res.json())
     .then((online) => {
       this.setState({ devices: online.devices });
@@ -83,7 +85,8 @@ class LoadDevices extends React.Component {
 
     const device = newDevices[index];
 
-    fetch('http://UniFrame.local:235/name', 
+    // fetch('http://UniFrame.local:235/name', 
+    fetch('http://79.67.44.40:235/name', 
     {
       method: 'POST',
       body: JSON.stringify({
@@ -101,7 +104,8 @@ class LoadDevices extends React.Component {
   }
 
   handleClickSet(device_id, action) {
-    fetch('http://UniFrame.local:235/device?id=' + device_id, 
+    // fetch('http://UniFrame.local:235/device?id=' + device_id, 
+    fetch('http://79.67.44.40:235/device?id=' + device_id, 
     {
       method: 'POST',
       body: JSON.stringify({
@@ -113,7 +117,8 @@ class LoadDevices extends React.Component {
   }
 
   handleClickToggle(device_id) {
-    fetch('http://UniFrame.local:235/device?id=' + device_id, 
+    // fetch('http://UniFrame.local:235/device?id=' + device_id, 
+    fetch('http://79.67.44.40:235/device?id=' + device_id, 
     {
       method: 'POST',
       body: JSON.stringify({
@@ -124,7 +129,8 @@ class LoadDevices extends React.Component {
   }
 
   handleConfigClick(device_id, index) {
-    fetch('http://UniFrame.local:235/connected?id=' + device_id)
+    // fetch('http://UniFrame.local:235/connected?id=' + device_id)
+    fetch('http://79.67.44.40:235/connected?id=' + device_id)
     .then(res => res.json())
     .then((data) => {
       const newDevices = this.state.devices.slice();
@@ -164,7 +170,8 @@ class LoadDevices extends React.Component {
     newDevices[index].config = "";
     this.setState({devices: newDevices});
     
-    fetch('http://UniFrame.local:235/connected?id=' + device_id, 
+    // fetch('http://UniFrame.local:235/connected?id=' + device_id, 
+    fetch('http://79.67.44.40:235/connected?id=' + device_id, 
     {
       method: 'POST',
       body: JSON.stringify({
